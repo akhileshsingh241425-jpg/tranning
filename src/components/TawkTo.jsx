d226111da33adf399dd1584e304726648c7b7e73
HRM Face Verification <hrm@example.com>
2026-06-21 14:57:42 +0530

    Hardcode Tawk.to ID in component (env var not working on server)

--- src/components/TawkTo.jsx
+++ src/components/TawkTo.jsx
@@ -2,13 +2,10 @@ import { useEffect } from 'react';
 
 const TawkTo = () => {
   useEffect(() => {
-    const propertyId = process.env.REACT_APP_TAWK_ID;
-    if (!propertyId) return;
-
     const s1 = document.createElement('script');
     const s0 = document.getElementsByTagName('script')[0];
     s1.async = true;
-    s1.src = `https://embed.tawk.to/${propertyId}`;
+    s1.src = 'https://embed.tawk.to/5f2c0161ed9d9d262708b09a';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);

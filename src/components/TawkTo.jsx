09ce5c28b922570b3d5b003ed7d6f7bb3690c69e
HRM Face Verification <hrm@example.com>
2026-06-21 15:03:29 +0530

    Fix Tawk.to: add /default URL and Tawk_API init

--- src/components/TawkTo.jsx
+++ src/components/TawkTo.jsx
@@ -2,10 +2,13 @@ import { useEffect } from 'react';
 
 const TawkTo = () => {
   useEffect(() => {
+    window.Tawk_API = window.Tawk_API || {};
+    window.Tawk_LoadStart = new Date();
+
     const s1 = document.createElement('script');
     const s0 = document.getElementsByTagName('script')[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/5f2c0161ed9d9d262708b09a';
+    s1.src = 'https://embed.tawk.to/5f2c0161ed9d9d262708b09a/default';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
